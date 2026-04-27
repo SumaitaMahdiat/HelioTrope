4d6a0cadd65c08cc37f3c2c75ce98c780e069a25
rishathamidi <hamidirisat@gmail.com>
2026-04-27 16:01:30 +0600

    Delete routes directory

--- routes/searchRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { searchProducts } from '../controllers/searchController.js';
-
-const router = Router();
-router.get('/', searchProducts);
-export default router;
